4d22c9232a8c8b737e9d20e788d1fc56ac3b4fb4
Alexander Melo <alexandermelox@gmail.com>
2018-10-22 18:06:57 -0400

    finished gulpfile tasks

--- gulpfile.js
+++ gulpfile.js
@@ -17,3 +17,15 @@ gulp.task("sass", function() {
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
+
+// Watch Sass & Server
+gulp.task("serve", ["sass"], function() {
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch(["src/scss/*.scss"], ["sass"]);
+  gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", ["serve"]);
